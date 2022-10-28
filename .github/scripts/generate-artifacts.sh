3cb1193350814e7317a2f4e05c82d2ec342028e4
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2022-10-28 18:50:08 +0200

    Automatically generate networks.json

---
+++ .github/scripts/generate-artifacts.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+repo_root_dir='.'
+deployment_output_folder="$repo_root_dir/broadcast/Deploy.sol"
+
+echo "Creating build artifacts..."
+forge build -o artifacts
+
+echo "Creating networks.json..."
+for deployment in "$deployment_output_folder/"*; do
+  # The subfolder name is the chain id
+  chain_id=${deployment##*/}
+  # First, every single deployment is formatted as if it had its own networks.json
+  jq --arg chainId "$chain_id" '
+    .transactions[]
+    | select(.transactionType == "CREATE" )
+    | {(.contractName): {($chainId): {address: .contractAddress, transactionHash: .hash }}}
+  '  <"$deployment/deployment.json"
+  # Then, all these single-contract single-chain-id networks.jsons are merged
+done \
+  | jq -n 'reduce inputs as $item ({}; . *= $item)' \
+  > "$repo_root_dir/networks.json"
